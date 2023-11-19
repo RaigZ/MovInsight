0457448ef51f2e0deb9a8c361b9f48e5c71443af
RaigZ <ragirzebari123@gmail.com>
2023-11-18 22:00:36 -0800

    Add movie contents into recyclerView
    Set data for dataList for what is retreived from api query string
    and put it in the RecyclerView component.

--- app/src/main/java/com/example/movinsight/DataAPIItem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.movinsight
-
-data class DataAPIItem(
-    val completed: Boolean,
-    val id: Int,
-    val title: String,
-    val userId: Int
-)
--- app/src/main/java/com/example/movinsight/RecyclerView/RVAdapter.kt
+++ app/src/main/java/com/example/movinsight/RecyclerView/RVAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.movinsight.RecyclerView
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,23 +9,35 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.example.movinsight.DataAPIItem
+import com.example.movinsight.API.SearchMovieResponse
+import com.example.movinsight.API.searchItem
 import com.example.movinsight.R
 import com.example.movinsight.RemoveActivity
 
-class RVAdapter(private val dataList: List<DataAPIItem>) : RecyclerView.Adapter<RVAdapter.ViewHolder>() {
+class RVAdapter(private var dataList: List<searchItem>) : RecyclerView.Adapter<RVAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RVAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_design, parent, false)
         return ViewHolder(view)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = dataList[position]
         holder.tvid.text = "ID: ${item.id.toString()}"
-        holder.tvuserid.text = "UserID: ${item.id.toString()}"
-        holder.tvtitle.text = "Title: ${item.title.toString()}"
-        holder.tvcompleted.text = "Completed: ${item.completed.toString()}"
+        holder.tvqid.text = "QID: ${item.id.toString()}"
+        holder.tvtitle.text = "TITLE: ${item.title.toString()}"
+        holder.tvyear.text = "YEAR: ${item.year.toString()}"
+        holder.tvstars.text = "STARS: ${item.stars.toString()}"
+        holder.tvq.text = "Q: ${item.q.toString()}"
+        holder.imageURL.text = "IMAGE URL: ${item.image.toString()}"
+
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(newSearchItems: SearchMovieResponse) {
+        dataList = newSearchItems.data
+        notifyDataSetChanged()
     }
 
     override fun getItemCount() : Int {
@@ -33,21 +46,32 @@ class RVAdapter(private val dataList: List<DataAPIItem>) : RecyclerView.Adapter<
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tvid: TextView = itemView.findViewById(R.id.tvid)
-        val tvuserid: TextView = itemView.findViewById(R.id.tvuserid)
+        val tvqid: TextView = itemView.findViewById(R.id.tvqid)
         val tvtitle: TextView = itemView.findViewById(R.id.tvtitle)
-        val tvcompleted: TextView = itemView.findViewById(R.id.tvcompleted)
+        val tvyear: TextView = itemView.findViewById(R.id.tvyear)
+        val tvstars: TextView = itemView.findViewById(R.id.tvstars)
+        val tvq: TextView = itemView.findViewById(R.id.tvq)
+        val imageURL: TextView = itemView.findViewById(R.id.imageURL)
 
         init {
             itemView.setOnClickListener {
-                Log.d("In RVAdapter", "${dataList[adapterPosition].id}")
-                Log.d("In RVAdapter", "${dataList[adapterPosition].userId}")
-                Log.d("In RVAdapter", "${dataList[adapterPosition].title}")
-                Log.d("In RVAdapter", "${dataList[adapterPosition].completed}")
+                Log.d("In RVAdapter", dataList[adapterPosition].id)
+                Log.d("In RVAdapter", dataList[adapterPosition].qid)
+                Log.d("In RVAdapter", dataList[adapterPosition].title)
+                Log.d("In RVAdapter", "${dataList[adapterPosition].year}")
+                Log.d("In RVAdapter", dataList[adapterPosition].stars)
+                Log.d("In RVAdapter", dataList[adapterPosition].q)
+                Log.d("In RVAdapter", dataList[adapterPosition].image)
+
                 val intent = Intent(itemView.context, RemoveActivity::class.java)
                 intent.putExtra("id", dataList[adapterPosition].id)
-                intent.putExtra("userId", dataList[adapterPosition].userId)
+                intent.putExtra("qid", dataList[adapterPosition].qid)
                 intent.putExtra("title", dataList[adapterPosition].title)
-                intent.putExtra("completed", dataList[adapterPosition].completed)
+                intent.putExtra("year", dataList[adapterPosition].year)
+                intent.putExtra("stars", dataList[adapterPosition].stars)
+                intent.putExtra("q", dataList[adapterPosition].q)
+                intent.putExtra("image", dataList[adapterPosition].image)
+
                 itemView.context.startActivity(intent)
                 Toast.makeText(itemView.context, "You have clicked ${dataList[adapterPosition]}", Toast.LENGTH_LONG).show()
             }
