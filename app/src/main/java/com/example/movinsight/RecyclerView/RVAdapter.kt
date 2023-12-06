a55bf7edd21ad8f87077cbdf1c73237847011fb1
RaigZ <ragirzebari123@gmail.com>
2023-12-06 03:03:58 -0800

    Declutter RecyclerView

--- app/src/main/java/com/example/movinsight/RecyclerView/RVAdapter.kt
+++ app/src/main/java/com/example/movinsight/RecyclerView/RVAdapter.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movinsight.API.SearchMovieResponse
 import com.example.movinsight.API.searchItem
@@ -24,15 +23,9 @@ class RVAdapter(private var dataList: List<searchItem>) : RecyclerView.Adapter<R
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = dataList[position]
-        holder.tvid.text = "ID: ${item.id.toString()}"
-        // Is the line below need to be displayed?
-        //holder.tvqid.text = "QID: ${item.id.toString()}"
         holder.tvtitle.text = "TITLE: ${item.title.toString()}"
         holder.tvyear.text = "YEAR: ${item.year.toString()}"
         holder.tvstars.text = "STARS: ${item.stars.toString()}"
-        holder.tvq.text = "Q: ${item.q.toString()}"
-       // holder.imageURL.text = "IMAGE URL: ${item.image.toString()}"
-
     }
 
     @SuppressLint("NotifyDataSetChanged")
@@ -46,13 +39,9 @@ class RVAdapter(private var dataList: List<searchItem>) : RecyclerView.Adapter<R
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvid: TextView = itemView.findViewById(R.id.tvid)
-        //val tvqid: TextView = itemView.findViewById(R.id.tvqid)
         val tvtitle: TextView = itemView.findViewById(R.id.tvtitle)
         val tvyear: TextView = itemView.findViewById(R.id.tvyear)
         val tvstars: TextView = itemView.findViewById(R.id.tvstars)
-        val tvq: TextView = itemView.findViewById(R.id.tvq)
-        //val imageURL: TextView = itemView.findViewById(R.id.imageURL)
 
         init {
             itemView.setOnClickListener {
@@ -76,7 +65,6 @@ class RVAdapter(private var dataList: List<searchItem>) : RecyclerView.Adapter<R
                 intent.putExtra("image", item.image)
                  */
                 itemView.context.startActivity(intent)
-                Toast.makeText(itemView.context, "You have clicked $item", Toast.LENGTH_LONG).show()
             }
         }
     }
